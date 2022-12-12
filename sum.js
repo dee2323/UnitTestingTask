b715052c7243001d35e2d769036f890674748213
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2022-12-12 19:27:40 +0200

    handle unknown amount of numbers and test it

--- sum.js
+++ sum.js
@@ -1,6 +1,15 @@
 const sum = (input) => {
     //check if null string 
     if (input.length === 0) return 0;
-    else { }
+    else {
+        let numbers = input.split(',')
+        let length = numbers.length
+        if (length < 3) {
+            numbers = numbers.map(number => Number(number))
+
+        } else return 'not allowed more than two numbers to add'
+    }
+
+
 }
 module.exports = sum;
--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ test('probably empty string it will return 0', () => {
     const str = ''
     expect(sum(str)).toBe(0)
 })
+test('probably more than two numbers to add not allowed', () => {
+    const str = '1,2,3'
+    expect(sum(str)).toBe('not allowed more than two numbers to add')
+})
