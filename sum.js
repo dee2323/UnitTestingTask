ad4ee244d4f99f2553e54f23fd117b649492de97
dee2323 <d.k.isleem@students.ptuk.edu.ps>
2022-12-12 19:32:53 +0200

    handle addition when the length one and two and test each case

--- sum.js
+++ sum.js
@@ -6,6 +6,10 @@ const sum = (input) => {
         let length = numbers.length
         if (length < 3) {
             numbers = numbers.map(number => Number(number))
+            switch (length) {
+                case 1: { return numbers[0] }
+                case 2: { return numbers[0] + numbers[1] }
+            }
 
         } else return 'not allowed more than two numbers to add'
     }
--- sum.test.js
+++ sum.test.js
@@ -7,3 +7,11 @@ test('probably more than two numbers to add not allowed', () => {
     const str = '1,2,3'
     expect(sum(str)).toBe('not allowed more than two numbers to add')
 })
+test('probably add two numbers', () => {
+    const str = '1,2'
+    expect(sum(str)).toBe(3)
+})
+test('probably  when the length is one', () => {
+    const str = '1'
+    expect(sum(str)).toBe(+str)
+})
